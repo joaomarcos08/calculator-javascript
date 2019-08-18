38ebce570ce4496b9a6dfc91670898e414d53a6f
joamarcos08 <joaomarcos1996@hotmail.com>
2019-08-17 22:42:17 -0300

    Importação inicial

---
+++ scripts/controler/calcControler.js
@@ -0,0 +1,95 @@
+class CalcControler {
+
+    constructor(){     
+
+        this._locale = "pt-Br";
+        this._displayCalcEL = document.querySelector("#display");
+        this._dateEL = document.querySelector("#data");
+        this._timeEL = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+        this.initButtonsEvents();
+
+    }
+
+    initialize(){
+
+        this.setDisplayDateTime();
+
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000);
+
+    }
+
+
+    addEventListenerAll(element, events, fn){
+
+        events.split(" ").forEach(event =>{
+
+            element.addEventListener(event, fn, false);
+
+        });
+
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+            
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=> {
+
+                btn.style.cursor = "pointer";
+
+            });
+
+        });
+    }    
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+    get displayTime(){
+        return this._timeEL.innerHTML;
+    }
+    
+    set displayTime(value){
+        this._timeEL.innerHTML =  value;
+    }
+
+    get displayDate(){
+        return this._dateEL.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEL.innerHTML= value;
+    }
+
+    get displdisplayCalcayDate(){
+        return _displayCalcEL.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEL.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+
+}
